e5776b83d64043fe053da48758182b15c9e042c8
정동순(Jung <a201903067@jungdongsoon.local>
2019-04-28 13:59:41 +0900

    debug decorator develope for common use

---
+++ studycafe/decorators.py
@@ -0,0 +1,12 @@
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr  = [repr(a) for a in args] # repr: print object.  eval: the same as c-system command(ex. system("ls"))
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__!r}({signature})") # !r means repr() is used to represent the value
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")
+        return value
+    return wrapper_debug
