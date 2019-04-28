d74352aa9068791e819bf032d5bc05430f1bc72a
정동순(Jung <a201903067@jungdongsoon.local>
2019-04-29 00:01:32 +0900

    issue_003_greeter_fstring change age as Man age

--- studycafe/enter.py
+++ studycafe/enter.py
@@ -1,4 +1,4 @@
 def greeter(name :str, age :int)->str:
     """greeter function to New Comer"""
-    return f'Hello,{name}. You are {age}.  Welcome to studycafe!'
+    return f'Hello,{name}. You are Full {age-1}.  Welcome to studycafe!'
 
